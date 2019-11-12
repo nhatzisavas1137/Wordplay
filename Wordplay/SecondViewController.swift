65855fd304fdaa859a85937c4f6621528417522d
nhatzisavas1137 <nhatzisavas1137@stu.d214.org>
2019-11-12 11:33:44 -0600

    MVP Completed

--- Wordplay/SecondViewController.swift
+++ Wordplay/SecondViewController.swift
@@ -2,12 +2,27 @@
 //  SecondViewController.swift
 //  Wordplay
 //
-//  Created by Niko Hatzisavas on 11/11/19.
+//  Created by Niko Hatzisavas on 11/12/19.
 //  Copyright © 2019 Niko Hatzisavas. All rights reserved.
 //
 
 import UIKit
+class SecondViewController: UIViewController {
+    var ending: String! = nil
+    var beginning: String! = nil
+    @IBOutlet weak var label2: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .init(red: 20/255, green: 206/255, blue: 219/255, alpha: 1.0)
+        label2.text = beginning + ending
+    }
+    /*
+    // MARK: - Navigation
 
-class SecondViewController: NSObject {
-
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 }
--- Wordplay/ViewController.swift
+++ Wordplay/ViewController.swift
@@ -12,13 +12,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var textField1: UITextField!
     @IBOutlet weak var button1: UIButton!
+    var sentenceBeginning: String = "My uncle wants to go to the "
     override func viewDidLoad() {
         super.viewDidLoad()
         button1.setTitle("Create sentence", for: .normal)
         textField1.placeholder = "Type sententence ending"
-        label1.text = "My uncle wants to go to the..."
-        let sentenceEnding = textField1.text!
+        label1.text = sentenceBeginning + "..."
+        view.backgroundColor = .init(red: 20/255, green: 206/255, blue: 219/255, alpha: 1.0)
     }
-
-
+    @IBAction func button1Pressed(_ sender: Any) {
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let sentenceEnding = textField1.text!
+        let sentenceBeginning: String = "My uncle wants to go to the "
+        let NVC = segue.destination as! SecondViewController
+        NVC.ending = sentenceEnding
+        NVC.beginning = sentenceBeginning
+    }
 }
