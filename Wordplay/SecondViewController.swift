29be2d0caab7aa41f4eb0f7b33a39c999d65e51c
nhatzisavas1137 <nhatzisavas1137@stu.d214.org>
2019-11-19 11:39:15 -0600

    Stretch 2 complete

--- Wordplay/SecondViewController.swift
+++ Wordplay/SecondViewController.swift
@@ -12,19 +12,22 @@ class SecondViewController: UIViewController {
     var adjective2: String! = nil
     var noun2: String! = nil
     var beginning: String! = nil
+    var wordLookFor2: String! = nil
     @IBOutlet weak var label2: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .init(red: 20/255, green: 206/255, blue: 219/255, alpha: 1.0)
         label2.text = beginning
+        let action1 = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction) in}
+        if beginning.contains(wordLookFor2) {
+            let findWordAlert = UIAlertController(title: "Word Found!", message: "Your word was found in the sentence! 😄", preferredStyle: .alert)
+            findWordAlert.addAction(action1)
+            self.present(findWordAlert, animated: true, completion: nil)
+        }
+        else {
+            let findWordAlert = UIAlertController(title: "Word Not Found", message: "Your word was not found in the sentence 😭", preferredStyle: .alert)
+            findWordAlert.addAction(action1)
+            self.present(findWordAlert, animated: true, completion: nil)
+        }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 }
--- Wordplay/ViewController.swift
+++ Wordplay/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UIApplicationDelegate {
     @IBOutlet weak var textField2: UITextField!
     @IBOutlet weak var textField1: UITextField!
     @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var textField4: UITextField!
     @IBOutlet weak var button1: UIButton!
     var adjective1 = "(adjective)"
     var verb1 = "(verb)"
@@ -20,12 +21,13 @@ class ViewController: UIViewController, UIApplicationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         let sentenceBeginning: String = "My " + adjective1 + " " + noun1 + " wants to " + verb1
-        button1.setTitle("Create sentence", for: .normal)
-        textField1.placeholder = "Type adjective"
+        button1.setTitle("Create Sentence", for: .normal)
+        textField1.placeholder = "Type Adjective"
         label1.text = sentenceBeginning
         view.backgroundColor = .init(red: 20/255, green: 206/255, blue: 219/255, alpha: 1.0)
-        textField2.placeholder = "Type noun"
-        textField3.placeholder = "Type verb"
+        textField2.placeholder = "Type Noun"
+        textField3.placeholder = "Type Verb"
+        textField4.placeholder = "Look For Word"
     }
     @IBAction func button1Pressed(_ sender: Any) {
     }
@@ -33,11 +35,13 @@ class ViewController: UIViewController, UIApplicationDelegate {
         let adjective1 = textField1.text!
         let verb1 = textField3.text!
         let noun1 = textField2.text!
+        let wordLookFor1 = textField4.text!
         let sentenceBeginning: String = "My " + adjective1 + " " + noun1 + " wants to " + verb1
         let NVC = segue.destination as! SecondViewController
         NVC.adjective2 = adjective1
         NVC.verb2 = verb1
         NVC.noun2 = noun1
+        NVC.wordLookFor2 = wordLookFor1
         NVC.beginning = sentenceBeginning
     }
 }
